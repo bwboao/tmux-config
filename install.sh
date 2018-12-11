f307104e1695f2cdf26614be2f6d02ad251f0fd8
bwboao <ben3226@gmail.com>
2018-12-12 06:04:53 +0800

    remove,debug install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-ln -S tmux-config-file ~/.tmux.conf
+
+#remove if .tmux exist
+tfile=${HOME}"/.tmux.conf"
+
+if [ -s ${tfile} ]; then
+    mv ${tfile} ${tfile}.old
+elif [ -e ~/.tmux.conf ]; then
+    rm ${tfile}
+fi
+
+#link softlink
+ln -s ${PWD}/tmux-config-file ${tfile}
